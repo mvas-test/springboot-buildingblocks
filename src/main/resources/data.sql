5e3e35266909b4a0734627813e1fe7000e20597c
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2020-04-08 12:17:24 +0100

    JPA Base RestServices Development Completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO USER (ID, EMAIL_ADDRESS, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME) VALUES
+	(101, 'mike@test.com', 'Mike', 'Vas', 'admin', 'ssn101', 'mvas'),
+	(102, 'micky@test.com', 'Micky', 'Test', 'admin', 'ssn102', 'mtest'),
+	(103, 'luda@test.com', 'Luda', 'Chic', 'user', 'ssn103', 'lchi');
