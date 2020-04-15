7f7dc8f17c16b1a0a0d5d8eb6a6adeb0512cfcc6
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2020-04-15 12:16:20 +0100

    added test orders entries

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,10 @@ INSERT INTO USER (ID, EMAIL_ADDRESS, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME
 	(101, 'mike@test.com', 'Mike', 'Vas', 'admin', 'ssn101', 'mvas'),
 	(102, 'micky@test.com', 'Micky', 'Test', 'admin', 'ssn102', 'mtest'),
 	(103, 'luda@test.com', 'Luda', 'Chic', 'user', 'ssn103', 'lchi');
+	
+INSERT INTO orders VALUES (2001, 'order11', 101);
+INSERT INTO orders VALUES (2002, 'order12', 101);
+INSERT INTO orders VALUES (2003, 'order13', 101);
+INSERT INTO orders VALUES (2004, 'order14', 102);
+INSERT INTO orders VALUES (2005, 'order15', 102);
+INSERT INTO orders VALUES (2006, 'order16', 103);
